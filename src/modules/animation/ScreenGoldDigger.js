24b5539a47acd0e76c6eb4fbd091ce8103223b81
Do The Anh <dotheanh12399@gmail.com>
2021-05-17 09:56:43 +0700

    refactor code + add screen evenlistener + calculate speed when pull up

--- src/modules/animation/ScreenGoldDigger.js
+++ src/modules/animation/ScreenGoldDigger.js
@@ -18,46 +18,46 @@ var ScreenGoldDigger = cc.Layer.extend({
         this.claw = null;
         this.itemSprites = [];
         this._super();
-        var size = cc.director.getVisibleSize();
+        this.scrSize = cc.director.getVisibleSize();
 
         // add background
         this.background = cc.Sprite.create("assests/game/images/background-sheet0.png");
-        this.background.setScale(size.width/this.background.getContentSize().width)
+        this.background.setScale(this.scrSize.width/this.background.getContentSize().width)
         this.background.attr({
-            x: size.width/2,
-            y: size.height/2
+            x: this.scrSize.width/2,
+            y: this.scrSize.height/2
         });
         this.background.setLocalZOrder(0);
         this.addChild(this.background);
 
         // SCALE RATE
-        this.SCALE_RATE = size.width/this.background.getContentSize().width
+        this.SCALE_RATE = this.scrSize.width/this.background.getContentSize().width
 
-        var btnGenerate = gv.commonButton(100, 34, size.width - 80, size.height - 52,"Generate");
+        var btnGenerate = gv.commonButton(100, 34, this.scrSize.width - 80, this.scrSize.height - 52,"Generate");
         btnGenerate.setTitleFontSize(15);
         this.addChild(btnGenerate);
-        btnGenerate.addClickEventListener(this.generateItem.bind(this));
-        
+        btnGenerate.addClickEventListener(this.onStartGame.bind(this));
 
-        var btnReset = gv.commonButton(100, 34, 70, size.height - 30,"Reset");
+
+        var btnReset = gv.commonButton(100, 34, 70, this.scrSize.height - 30,"Reset");
         btnReset.setTitleFontSize(15);
         this.addChild(btnReset);
         btnReset.addClickEventListener(this.onSelectReset.bind(this));
 
 
-        var xPos = (size.width - 220)/2;
+        var xPos = (this.scrSize.width - 220)/2;
         this.score = 0;
-        this.scoreBox = gv.commonText(fr.Localization.text("0"), size.width*1/6, size.height-size.height/6);
+        this.scoreBox = gv.commonText(fr.Localization.text("0"), this.scrSize.width*1/6, this.scrSize.height-this.scrSize.height/6);
         this.addChild(this.scoreBox);
 
-        this.countdownBox = gv.commonText(fr.Localization.text("01:00"), size.width*1/6, size.height-size.height/10);
+        this.countdownBox = gv.commonText(fr.Localization.text("01:00"), this.scrSize.width*1/6, this.scrSize.height-this.scrSize.height/10);
         this.addChild(this.countdownBox);
 
         // add character
         this.character = cc.Sprite.create("assests/game/images/excavator-sheet0.png", cc.rect(0,0,512,180));
         this.character.attr({
             x: xPos,
-            y: size.height - size.height*0.24
+            y: this.scrSize.height - this.scrSize.height*0.24
         });
         this.character.setAnchorPoint(0,0.5);
         this.addChild(this.character);
@@ -93,70 +93,35 @@ var ScreenGoldDigger = cc.Layer.extend({
                 if (this.checkTouchItem(sprite)) {
                     this.ableToTouchItem = false;
                     this.claw.stopAction(this.normalClawCycle);
-                    this.absolutelyReturnClawAction = cc.moveTo(2, this.initClawX, this.initClawY); /// khai báp trùng lặp
-                    sprite.runAction(cc.moveTo(2, this.initClawX, this.initClawY));
-                    this.itemSprites.splice(index, 1);//////////////////
+                    let distance = this.calDistance(sprite.getPositionX(), sprite.getPositionY(), this.initClawX, this.initClawY);
+                    const v = 80; // tốc độ kéo lên
+                    let t = distance/v;
+                    console.log("distance: " + distance);
+                    console.log("t: " + t);
+                    this.absolutelyReturnClawAction = cc.moveTo(t, this.initClawX, this.initClawY); /// khai báp trùng lặp
+                    sprite.runAction(cc.moveTo(t+0.5, this.initClawX, this.initClawY));
                     this.claw.runAction(cc.sequence(this.absolutelyReturnClawAction,cc.callFunc(this.onNormalReturnClaw, this),cc.callFunc(()=>{sprite.getParent().removeChild(sprite,true);}),cc.callFunc(()=>{this.scoreBox.setString(++this.score)})));
+                    this.itemSprites.splice(index, 1);
                 }
             })
         }
     },
-    // testAnimationBinding:function()
-    // {
-    //     if(this.character)
-    //         this.character.removeFromParent();
-    //     this.character = fr.createAnimationById(resAniId.chipu,this);
-    //     this.nodeAnimation.addChild(this.character);
-    //     this.character.setPosition(cc.p(0,0));
-    //     this.character.setScale(2);
-    //     this.character.getAnimation().gotoAndPlay("win_0_",-1,-1,1);
-    //     this.character.setCompleteListener(function () {
-    //         this.testAnimationBinding();
-    //     }.bind(this));
-
-    // },
-    // testPlayAnimation:function()
-    // {
-    //     if(this.character)
-    //         this.character.removeFromParent(true);
-
-    //     this.character = fr.createAnimationById(resAniId.chipu,this);
-    //     //doi mau, yeu cau phai co file shader, nhung bone co ten bat dau tu color_ se bi doi mau
-    //     this.character.setBaseColor(255,255,0);
-    //     //chinh toc do play animation
-    //     this.character.getAnimation().setTimeScale(0.5);
-    //     this.nodeAnimation.addChild(this.character);
-    //     //play animation gotoAndPlay(animationName, fadeInTime, duration, playTimes)
-    //     this.character.getAnimation().gotoAndPlay("idle_0_",-1);
-
-    // },
     throwClaw:function()
     {
-        // if(this.character)
-        //     this.character.removeFromParent();
-        // this.character = fr.createAnimationById(resAniId.chipu,this);
-        // this.nodeAnimation.addChild(this.character);
-        // this.character.getAnimation().gotoAndPlay("win_0_",-1,-1,1);
-        // this.character.setCompleteListener(this.onFinishAnimations.bind(this));
-        
         this.onThrowClaw();
-    },
-
-    // onFinishAnimations:function()
-    // {
-    //     this.character.getAnimation().gotoAndPlay("idle_0_",0);
-    // },
-    // onFinishEffect:function(animation)
-    // {
-    //     animation.removeFromParent();
-    // },
-    generateItem:function()
+    },   
+    onStartGame:function()
     {
+        this.initTheClaw();
+        this.generateItem();
+        this.startCountDown();
+        this.addMiningListener();
+    },
+    initTheClaw:function() {
         // hiện móc câu
-        let scrSize = cc.director.getVisibleSize();
         this.claw = cc.Sprite.create("assests/game/images/hook-sheet0.png");
-        this.initClawX = (scrSize.width - 220)/2;
-        this.initClawY = scrSize.height - scrSize.height*0.21;
+        this.initClawX = (this.scrSize.width - 220)/2;
+        this.initClawY = this.scrSize.height - this.scrSize.height*0.21;
         this.claw.attr({
             x: this.initClawX,
             y: this.initClawY
@@ -171,7 +136,9 @@ var ScreenGoldDigger = cc.Layer.extend({
         var rotateClawRL = cc.rotateBy(2, angle);
         this.rotatingAction = cc.sequence(initRotateClaw, cc.repeat(cc.sequence(rotateClawLR, rotateClawRL), 99));
         this.claw.runAction(this.rotatingAction);
-
+    },
+    generateItem:function()
+    {
         // generate random gold and diamond
         let itemsCount = 100;
         let itemsCoord = []; // ghi lại tọa độ các item đã tồn tại để tránh trùng tọa độ
@@ -204,8 +171,8 @@ var ScreenGoldDigger = cc.Layer.extend({
             do {
                 randomCount++;
                 newCoord = {
-                    xPosition: randomInt(40, scrSize.width-40), // random( 40 to size.width-40)
-                    yPosition: randomInt(40, scrSize.height - scrSize.height/2.2) // random( size.height/4 to size.height - size.height/3)
+                    xPosition: randomInt(40, this.scrSize.width-40), // random( 40 to this.scrSize.width-40)
+                    yPosition: randomInt(40, this.scrSize.height - this.scrSize.height/2.2) // random( this.scrSize.height/4 to this.scrSize.height - this.scrSize.height/3)
                 }
                 itemsCoord.forEach(coord => {
                     if (Math.abs(coord.xPosition - newCoord.xPosition) < 50 && Math.abs(coord.yPosition - newCoord.yPosition) < 50) { // quá gần nhau
@@ -228,6 +195,8 @@ var ScreenGoldDigger = cc.Layer.extend({
 
         }
 
+    },
+    startCountDown:function() {
         // hiển thị thời gian đếm ngược
         var timer = duration, minutes, seconds;
         const countdownBox = this.countdownBox;
@@ -238,44 +207,35 @@ var ScreenGoldDigger = cc.Layer.extend({
             minutes = minutes < 10 ? "0" + minutes : minutes;
             seconds = seconds < 10 ? "0" + seconds : seconds;
 
-            //display.textContent = minutes + ":" + seconds;/// update tẽt
             countdownBox.setString(minutes + ":" + seconds);
 
             if (--timer < 0) {
                 countdownBox.setString("TIME OUT");
             }
         }, 1000);
-
-        // hiển thị nút đào vàng
-        var btnThrowClaw = gv.commonButton(100, 34, scrSize.width - 80, scrSize.height - 132,"Dig it!");
-        btnThrowClaw.setTitleFontSize(15);
-        this.addChild(btnThrowClaw);
-        btnThrowClaw.addClickEventListener(this.throwClaw.bind(this));   // testFinishAnimationEvent: do animation
-
-        // const cThis = this;
-        // this.digScreen = new cc.Node();
-        // this.digScreen.attr({
-        //     x: 0,
-        //     y: 0
-        // });
-        // this.digScreen.setContentSize(cc.size(scrSize.width,scrSize.height - scrSize.height/2.2));
-        // //digScreen.addClickEventListener(this.testFinishAnimationEvent.bind(this));   // testFinishAnimationEvent: do animation
-        // cc.eventManager.addListener({
-        //     event: cc.EventListener.MOUSE,
-        //     onMouseDown: function(event){
-        //         cThis.testFinishAnimationEvent.bind(cThis);
-        //     }
-        // }, cThis.digScreen);
-        // this.addChild(this.digScreen);
+    },
+    addMiningListener:function() {
+        // add event listener for layer
+        const cThis = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            onTouchBegan: function(touch, event){
+                // check if touched in mining area
+                if (40 < touch.getLocation().x && touch.getLocation().x < cThis.scrSize.width-40 && 40 < touch.getLocation().y && touch.getLocation().y < cThis.scrSize.height - cThis.scrSize.height/2.2) {
+                    cThis.throwClaw();
+                }
+                return true;
+            },
+        }, this);
+        
     },
     onThrowClaw:function()
     {
         this.ableToTouchItem = true;
         // di chuyển móc câu
-        let scrSize = cc.director.getVisibleSize();
         let radian = this.angle/360 * 2 * Math.PI;
-        let deltaX = Math.cos(radian)*scrSize.width/1.5;
-        let deltaY = Math.sin(radian)*scrSize.width/1.5;
+        let deltaX = Math.cos(radian)*this.scrSize.width/2;
+        let deltaY = Math.sin(radian)*this.scrSize.width/2;
         this.claw.stopAction(this.rotatingAction);
         this.throwClawAction = cc.moveBy(2, deltaX, deltaY);
         this.normalClawCycle = cc.sequence(this.throwClawAction, cc.callFunc(this.onNormalReturnClaw, this))
@@ -291,7 +251,7 @@ var ScreenGoldDigger = cc.Layer.extend({
         var rotateClawRL = cc.rotateBy(2, angle);
         this.rotateAction = cc.sequence(backtoRotationClaw, cc.repeat(cc.sequence(rotateClawLR, rotateClawRL), 99));
         this.rotatingAction = cc.sequence(cc.delayTime(this.absolutelyReturnClawAction.getDuration()), this.rotateAction);
-        this.claw.runAction(this.absolutelyReturnClawAction);    //=> ko thể stop rotatingAction!!!!!!!!
+        this.claw.runAction(this.absolutelyReturnClawAction);
         this.claw.runAction(this.rotatingAction);
     },
     checkTouchItem: function(item){
@@ -299,9 +259,14 @@ var ScreenGoldDigger = cc.Layer.extend({
         let y = this.claw.getPositionY();
         let itemX = item.getPositionX();
         let itemY = item.getPositionY();
-        let itemWidth = item.getBoundingBox().height/2;
-        let itemHeight = item.getBoundingBox().width/2;
+        let itemWidth = item.getBoundingBox().height/1.3;
+        let itemHeight = item.getBoundingBox().width/1.3;
         return ((itemX - itemWidth <= x && x <= itemX + itemWidth) && (itemY - itemHeight <= y && y <= itemY + itemHeight));
+    },
+    calDistance: function(x1, y1, x2, y2) {
+        let deltaX = x1 - x2;
+        let deltaY = y1 - y2;
+        return Math.sqrt(deltaX*deltaX + deltaY*deltaY);
     }
 
 });
